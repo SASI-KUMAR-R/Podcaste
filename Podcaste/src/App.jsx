4d3375fadc34d094935c5960d33572f30114c7a9
SASIKUMAR R <rsasikumar670@gmail.com>
2025-02-28 14:12:57 +0530

    "OVER SIGNUP AND LOGIN FRONT & BACKEND DONE"

--- Podcaste/src/App.jsx
+++ Podcaste/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./Components/FunctionComponents/Navbar";
+// import Navbar from "./Components/FunctionComponents/Navbar";
 import SignUp from "./Components/FunctionComponents/SignUp";
 import Home from "./Components/FunctionComponents/Home";
 import Login from "./Components/FunctionComponents/Login";
