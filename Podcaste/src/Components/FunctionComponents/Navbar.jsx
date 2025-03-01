ffbad7badd39308ca92814be31a6ad215641bc45
SASIKUMAR R <rsasikumar670@gmail.com>
2025-03-01 14:40:20 +0530

    "Logo Bug Fixed"

--- Podcaste/src/Components/FunctionComponents/Navbar.jsx
+++ Podcaste/src/Components/FunctionComponents/Navbar.jsx
@@ -2,14 +2,16 @@ import { useNavigate } from "react-router-dom";
 import "../CSS/Navbar.css";
 import { IoIosNotifications } from "react-icons/io";
 import ShinyText from "./ShinyText";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const navigate = useNavigate();
   return (
     <div className="container">
       <nav className="navbar">
-        <div className="logo"><a href="/Home" className="atag">PODCASTS</a></div>
-
+      <div className="logo">
+      <Link to="/Home" className="atag">PODCASTS</Link>
+    </div>
         <div className="searchbar">
           <input type="text" placeholder="SEARCH..." className="search-input" />
         </div>
@@ -18,15 +20,16 @@ function Navbar() {
           <button className="buttonstyle">
             <IoIosNotifications className="iconsstyle" />
           </button>
-          <button
-            className="buttonstyle2"
-            onClick={() => navigate("/")}
-          >
-            <ShinyText text="LOG OUT" disabled={false} speed={3} className='custom-class' />
+          <button className="buttonstyle2" onClick={() => navigate("/")}>
+            <ShinyText
+              text="LOG OUT"
+              disabled={false}
+              speed={3}
+              className="custom-class"
+            />
           </button>
         </div>
       </nav>
-      
     </div>
   );
 }
